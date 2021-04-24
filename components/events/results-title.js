acbc1dbfa0408b1318564d7f154e72d91da41dd1
keremcanb <keremcan@gmail.com>
2021-04-24 20:05:03 +0300

    m

--- components/events/results-title.js
+++ components/events/results-title.js
@@ -5,7 +5,7 @@ import classes from './results-title.module.css';
 const ResultsTitle = ({ date }) => (
   <section className={classes.title}>
     <h1>
-      Events in <Moment format="MMM">{date}</Moment>
+      Events in <Moment format="MMMM">{date}</Moment>
     </h1>
     <Button link="/events">Show all events</Button>
   </section>
